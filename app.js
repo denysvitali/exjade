1d7f22a2bfffa9da785853b260dcf97e31d8b890
Denys Vitali <denys.vitali@coelis.ch>
2015-10-07 11:21:44 +0200

    Fixed errors with older express 3 => Migrated app.js to express 4

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-var express = require('express')
-  , routes = require('./routes')
+var express = require('express'),
+    routes = require('./routes'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    errorHandler = require('errorhandler');
 
-var app = module.exports = express.createServer();
+var app = express();
 
-var cors = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+var cors = function(req, res, next)
+{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
 }
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(cors);
-  app.use(app.router);
-  app.set('view options', {layout: false})
-  app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded(
+{
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(cors);
+app.set('view options',
+{
+    layout: false
 });
+app.use(express.static(__dirname + '/public'));
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
+if (app.get('env') === "development")
+{
+    app.use(errorHandler(
+    {
+        dumpExceptions: true,
+        showStack: true
+    }));
+}
+else
+{
+    app.use(errorHandler());
+}
 
 // Routes
 
 app.get('/', routes.index);
 
-app.listen(3000);
-console.log("Express listening on port %d in %s mode", app.address().port, app.settings.env);
+var server = app.listen(3000);
+console.log("Express listening on port %d in %s mode", server.address().port, app.settings.env);
